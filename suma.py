eff4bf799efc9591de38fc7cf560552e18ecaa75
fernandoandresgc1109-web <fernandoandresgc1109@gmail.com>
2026-02-26 15:30:48 -0300

    Update suma.py

--- suma.py
+++ suma.py
@@ -6,5 +6,5 @@ def sumar(a, b):
 if __name__ == "__main__":
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
-    print("La suma es:", sumar(x, y))
+    print("La sumatoria es:", sumar(x, y))
 
