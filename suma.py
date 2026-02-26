762a630469c59166836cdd2487f34dcbe193ba67
Andrés <fernandoandresgc1109@gmail.com>
2026-02-26 15:29:35 -0300

    Corrijo suma.py con codificación UTF-8

---
+++ suma.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    x = int(input("Ingresa el primer número: "))
+    y = int(input("Ingresa el segundo número: "))
+    print("La suma es:", sumar(x, y))
+
